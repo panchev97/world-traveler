a3b084d8d9d28081675f73e7412b886e08c00be4
Rumen Panchev <rumen@sapirshop.com>
2019-05-19 01:57:17 +0300

    Add header and footer

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className='Header'>
+                <nav className='navbar navbar-light bg-light'>
+                    <span className='navbar-brand mb-0 h1'>Welcome to World Traveler</span>
+                </nav>
+            </div>
+        )
+    }
+}
+
+export default Header;
