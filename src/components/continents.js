e1cdd511b33d48500ccd67fcd23a702c5cc6dd5d
Rumen Panchev <rumen@sapirshop.com>
2019-05-19 11:08:22 +0300

    Add onContinentClick event

--- src/components/continents.js
+++ src/components/continents.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
-import { gql } from "apollo-boost";
+import { gql } from 'apollo-boost';
 import Loading from './loading';
 
 const pullContinentsQuery = gql`
@@ -13,24 +13,28 @@ const pullContinentsQuery = gql`
 ` 
 
 class Continents extends Component {
+    constructor (props) {
+        super(props);
+
+        this.onContinentClick = this.onContinentClick.bind(this);
+    }
+    onContinentClick(name) {
+        console.log(name);
+    }
     displayContinents() {
         let data = this.props.data;
         if (data.loading === true) {
-            return (
-                <div>
-                    <Loading />
-                </div>
-            )
+            return (<Loading />)
         } else {
             return data.continents.map(continent => {
                 return (
                     <li key={continent.name} className='list-group-item'>
-                        {continent.name} <span className='badge badge-primary badge-pill'>{continent.code}</span>
+                        {continent.name} <span className='badge badge-primary badge-pill'>{continent.code} </span>
+                        <a href='#' onClick={this.onContinentClick.bind(null, continent.name)} className='btn btn-outline-info ml-5'>Continent Info</a>
                     </li>
                 )
             });
         }
-
     }
     render() {
         return (
